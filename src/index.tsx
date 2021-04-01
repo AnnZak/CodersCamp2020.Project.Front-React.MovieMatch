3edeb4527807cd7fd57a5aa0c3aa0634270d574d
AleksandraZochowska <74739423+AleksandraZochowska@users.noreply.github.com>
2021-04-01 19:59:40 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-import Registration from './views/Registration/Registration'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Registration />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
