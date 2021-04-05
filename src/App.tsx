7f23b995fefbdcaec8fbbac11f1fc327fc335204
Aleksandra Zochowska <a.j.zochowska@gmail.com>
2021-04-05 23:39:56 +0200

    add forgot-password & reset-password form

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.scss';
-import Registration from './views/Registration/Registration'
+// import Registration from './views/Registration/Registration';
 // import Topbar from './components/layout/topbar/topbar';
 // import ExampleForm from './views/ExampleForm/ExampleForm';
+// import ForgotPassword from './views/ForgotPassword/ForgotPassword';
+// import ResetPassword from './views/ResetPassword/ResetPassword';
 
 function App() {
   return (
     <div className="App">
       {/* <Topbar /> */}
       {/* <ExampleForm /> */}
-      <Registration />
+      {/* <Registration /> */}
+      {/* <ForgotPassword /> */}
+      {/* <ResetPassword /> */}
     </div>
   );
 }
