c8dde96a2310cbd988f29372dbb7c8b547d4dec7
Cidebur <mcibor96@gmail.com>
2021-04-09 22:15:55 +0200

    Added user/login reducers

--- src/features/User/UserSlice.ts
+++ src/features/User/UserSlice.ts
@@ -2,35 +2,8 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { RootState } from '../../app/store';
 
-export const userSlice = createSlice({
-
-    name: 'user',
-    initialState: {
-        username: '',
-        email: '',
-        isFetching: false,
-        isSuccess: false,
-        isError: false,
-        errorMsg: '',
-    },
-    reducers: {
-        clearState: (state) => {
-            state.isError = false;
-            state.isSuccess = false;
-            state.isFetching = false;
-      
-            return state;
-        },
-    },
-    extraReducers: {
-
-    },
-})
-
-export const userSelector = (state: RootState) => state.user;
-
 export const loginUser = createAsyncThunk(
-    'users/login',
+    'user/login',
     async ( userCredentials: {login: string, password: string}, thunkApi) => {
         try {
 
@@ -52,5 +25,50 @@ export const loginUser = createAsyncThunk(
         }
     }
 );
+
+export const userSlice = createSlice({
+
+    name: 'user',
+    initialState: {
+        displayedName: '',
+        name: '',
+        email: '',
+        isFetching: false,
+        isSuccess: false,
+        isError: false,
+        errorMsg: '',
+    },
+    reducers: {
+        clearState: (state) => {
+            state.isError = false;
+            state.isSuccess = false;
+            state.isFetching = false;
+            return state;
+        },
+    },
+    extraReducers: (builder) => {
+        builder.addCase(loginUser.fulfilled, (state, { payload }) => {
+            state.displayedName = payload.user.displayedName;
+            state.name = payload.user.name;
+            state.email = payload.user.email;
+            state.isFetching = false;
+            state.isSuccess = true;
+            return state;
+        });
+
+        builder.addCase(loginUser.rejected, (state, { payload }) => {
+            console.log('payload', payload);
+            state.isFetching = false;
+            state.isError = true;
+            // state.errorMsg = payload.message;
+        });
+
+        builder.addCase(loginUser.pending, (state, { payload }) => {
+            state.isFetching = true;
+        });
+    }
+})
+
+export const userSelector = (state: RootState) => state.user;
 
 export const { clearState }  = userSlice.actions;
