2b8b2a9091ead90188c8355e58dc099c0c9f98f6
Aleksandra Zochowska <a.j.zochowska@gmail.com>
2021-04-14 00:02:31 +0200

    topbar dropdown changed to onclick from hover

--- src/components/layout/topbar/dropdown/dropdown.tsx
+++ src/components/layout/topbar/dropdown/dropdown.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './dropdown.scss';
 import avatardefault from '../../../../assets/images/avatardefault.png';
 import { Link, useHistory } from 'react-router-dom';
@@ -7,6 +7,7 @@ import { logout } from '../../../../features/User/UserSlice';
 
 const Dropdown = () => {
 
+    const [visible, setVisible] = useState(false);
     const dispatch = useAppDispatch();
     const history = useHistory();
 
@@ -15,12 +16,16 @@ const Dropdown = () => {
         history.push("/");
     }
 
+    const handleVisibility = () => {
+        setVisible(prev => !prev);
+    };
+
     return (
         <div className="topbar__dropdown">
             <ul>
                 <li>
-                    <a className="topbar__avatar-link" href="#"><img className="topbar__avatar-img" src={avatardefault} /></a>
-                    <ul className="topbar__dropdown-menu">
+                    <a className="topbar__avatar-link" href="#" onClick={handleVisibility}><img className="topbar__avatar-img" src={avatardefault} /></a>
+                    <ul className={visible ? "topbar__dropdown-menu visible" : "topbar__dropdown-menu invisible"}>
                         <li className="topbar__dropdown-menu__item"><Link to="/settings">Settings</Link></li>
                         <li className="topbar__dropdown-menu__item"><button onClick={handleLogOut}>Log Out</button></li>
                     </ul>
