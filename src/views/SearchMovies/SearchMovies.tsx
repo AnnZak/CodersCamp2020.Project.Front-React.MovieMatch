73a34b66ee9f87c73f152fe094d2bfb46c1a5927
Aleksandra Zochowska <a.j.zochowska@gmail.com>
2021-04-14 14:18:07 +0200

    add liking movies from search view & refac search to use movie card component

--- src/views/SearchMovies/SearchMovies.tsx
+++ src/views/SearchMovies/SearchMovies.tsx
@@ -1,56 +1,73 @@
-import './SearchMovies.scss';
-import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { unwrapResult } from '@reduxjs/toolkit';
 
+import './SearchMovies.scss';
 import Topbar from '../../components/layout/topbar/topbar';
+import MovieBriefCard from '../../components/layout/movieBriefCard/movieBriefCard';
+import { userSelector } from '../../features/User/UserSlice';
+import { movieSelector, removeFromLiked, searchMovies, addToLiked, getUserCollection, getMovieDetails, clearState } from '../../features/Movie/MovieSlice';
+import { MovieDetailsResponse } from '../../features/Movie/ts/movieTypes';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
-import { movieSelector, removeFromLiked, searchMovies, addToLiked } from '../../features/Movie/MovieSlice';
-import moviedefault from '../../assets/images/moviedefault.jpg';
 
 function SearchMovies(props: { location?: { search?: string } }) {
 
+    const displayedInitialState = {
+        movie: {
+            imdbId: "",
+            Title: "",
+            imdbRating: "",
+            Runtime: "",
+            Year: "",
+            Country: "",
+            Genre: "",
+            Director: "",
+            Actors: "",
+            Awards: "",
+            Plot: "",
+            Poster: "",
+        },
+        watched: false
+    }
+
+    const [errMessage, setErrMessage] = useState("");
+    const [displayedMovies, setDisplayedMovies] = useState<{ movie: MovieDetailsResponse, watched?: boolean }[]>([displayedInitialState]);
+
     const dispatch = useAppDispatch();
+    const { _id } = useAppSelector(userSelector);
     const { searchedMovies } = useAppSelector(movieSelector);
 
+    const searchBy = props.location?.search?.replace("?title=", "").replace("%20", "+");
     useEffect(() => {
-        const searchBy = props.location?.search?.replace("?title=", "").replace("%20", "+");
-        if (searchBy) dispatch(searchMovies(searchBy));
-    }, []);
+        setDisplayedMovies([displayedInitialState]);
+        setErrMessage("");
 
-    function handleToggleLiked(movieId: string) {
-        const toggle = async () => {
-            const liked = await dispatch(addToLiked(movieId));
-            if (liked.payload !== movieId) {
-                await dispatch(removeFromLiked(movieId));
+        if (searchBy) dispatch(searchMovies(searchBy)).then(unwrapResult).then(originalResult => {
+            if (!searchedMovies) setErrMessage("No movies found. Try searching different title.")
+            dispatch(clearState());
+            for (const movie of originalResult) {
+                dispatch(getUserCollection(_id));
+                dispatch(clearState());
+                dispatch(getMovieDetails(movie.imdbID)).then(unwrapResult).then(originalResult => {
+                    dispatch(clearState());
+                    setDisplayedMovies(state => [...state, { movie: originalResult }]);
+                }).catch(e => { setErrMessage("No movies found. Try searching different title.") });
             }
-        }
-        toggle();
-    }
+        }).catch(e => { setErrMessage("No movies found. Try searching different title.") });
+
+    }, [searchBy]);
 
     return (
         <div>
             <Topbar />
             <div className="searched-movies-container">
                 <div className="searched-movie-cards-container">
-                    {searchedMovies && searchedMovies[0].Title ?
-                        searchedMovies.map((movie) => (
-                            <div className="searched-movie-card">
-                                { (movie.Poster && movie.Poster !== "N/A") &&
-                                    <Link to={`movies/${movie.imdbID}`}><img className="searched-movie-poster" src={movie.Poster} alt="movie poster" /></Link>
-                                }
-                                { (movie.Poster === "N/A") &&
-                                    <img className="searched-movie-poster" src={moviedefault} alt="default movie poster" style={{ opacity: 0.5 }} />
-                                }
-                                { movie.Title &&
-                                    <div className="searched-movie-actions-icons">
-                                        <Link to={`movies/${movie.imdbID}`}><h2 className="searched-movie-title">{movie.Title}</h2></Link>
-                                        <button className="heart-collection liked" onClick={() => { handleToggleLiked(movie.imdbID) }}><i className="fas fa-heart"></i></button>
-                                        {/* <button onClick={() => { handleToggleWatched() }}><i className="far fa-eye"></i></button> */}
-                                    </div>
-                                }
-                            </div>
-                        )) :
-                        <h2>Search movie by title...</h2>
+                    {errMessage !== "" ?
+                        <div className="searched__error-container">
+                            <h1>{errMessage}</h1>
+                        </div> :
+                        displayedMovies.map((element) =>
+                            element.movie.Title && <MovieBriefCard el={element as { movie: MovieDetailsResponse }} />
+                        )
                     }
                 </div>
             </div>
