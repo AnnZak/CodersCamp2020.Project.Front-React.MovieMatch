bbdfd4f9c72249c37f423f6876ddc6472e51890c
Anna Zak <zak.anna.94@gmail.com>
2021-04-14 19:36:44 +0200

    added register confirm

--- src/views/RegisterConfirm/RegisterConfirm.tsx
+++ src/views/RegisterConfirm/RegisterConfirm.tsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import Topbar from "../../components/layout/topbar/topbar";
 import { clearState, confirmRegistration, userSelector } from "../../features/User";
+import './RegisterConfirm.scss';
+import TopLogo from '../../components/ui/topLogo/topLogo';
 
 function RegisterConfirm() {
 
@@ -32,7 +35,10 @@ function RegisterConfirm() {
       }, [isError, isSuccess]);
 
     return (
-        <h1>{error}</h1>
+      <div className ="wrong-token">
+        <TopLogo />
+        <h1 className="h1-error">{error}</h1>
+      </div>
     );
 }
 
