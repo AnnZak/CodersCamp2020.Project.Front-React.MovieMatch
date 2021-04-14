fe97fd8e0227b16fb053d8f5f577e21368ad3ac5
Cidebur <mcibor96@gmail.com>
2021-04-14 11:32:05 +0200

    Forgot password now renders api answer

--- src/views/ForgotPassword/ForgotPassword.tsx
+++ src/views/ForgotPassword/ForgotPassword.tsx
@@ -6,17 +6,29 @@ import {forgotPassword} from '../../features/User/api'
 
 function ForgotPassword() {
 
+    const initialInfo = `You will receive email with link to reset your password. Make sure to provide the email you registered to MovieMatch with.`
+
     const [email, setEmail] = useState("");
     const [emailWarning, setEmailWarning] = useState("");
+    const [info, setInfo] = useState(initialInfo);
 
-    const onSubmit = (data: string) => {
+    const onSubmit = async (data: string) => {
         const emailRegEx = /^[a-z\d]+[\w\d.-]*@(?:[a-z\d]+[a-z\d-]+\.){1,5}[a-z]{2,6}$/
 
         if(!emailRegEx.test(email)){
             setEmailWarning("(provide valid email)");
         } else {
             setEmailWarning("");
-            forgotPassword(email);
+            try {
+                const response = await forgotPassword(email);
+                if(response.status === 200) {
+                    setInfo(response.data.message);
+                } else {
+                    setInfo(response.data.error);
+                }
+            } catch (error) {
+                setInfo(error.response.data.error);
+            }
         };
     }
 
@@ -30,7 +42,7 @@ function ForgotPassword() {
                     { type: 'submit', value: 'Send email' }
                 ]}
                 onSubmit={() => { onSubmit(email) }}
-                info={`You will receive email with link to reset your password. Make sure to provide the email you registered to MovieMatch with.`}
+                info={info}
             />
         </div>
     );
