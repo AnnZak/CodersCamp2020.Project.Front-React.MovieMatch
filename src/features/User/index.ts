97f23c9a57bd49098af75e5c692d93f1d7a527b4
Cidebur <mcibor96@gmail.com>
2021-04-15 14:38:42 +0200

    changed userSlice name, fixed imports

--- src/features/User/index.ts
+++ src/features/User/index.ts
@@ -1,4 +1,4 @@
 export type { LoginCredentials, RegisterCredentials } from './types';
-export { userReducer, userSelector, clearState, resetState, logout } from './UserSlice';
+export { userReducer, userSelector, clearState, resetState, logout } from './userSlice';
 export { loginUser, getUserData, registerUser, confirmRegistration, changeData } from './thunks';
 export * as userApi from './api';
