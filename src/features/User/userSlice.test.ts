c4232f4e9e4acf38d2ed2fda6d74ea61733168cf
Cidebur <mcibor96@gmail.com>
2021-04-15 14:53:57 +0200

    Basic userSlice tests added

---
+++ src/features/User/userSlice.test.ts
@@ -0,0 +1,35 @@
+import {userReducer, clearState} from './userSlice'
+import {SliceState} from './types';
+
+describe('user reducer', () => {
+
+    const testState: SliceState = {
+        _id: '',
+        displayedName: '',
+        name: '',
+        email: '',
+        isFetching: true,
+        isSuccess: true,
+        isError: true,
+        errorMsg: 'Try Me!',
+    }
+    
+    const initialState: SliceState = {
+        _id: '',
+        displayedName: '',
+        name: '',
+        email: '',
+        isFetching: false,
+        isSuccess: false,
+        isError: false,
+        errorMsg: '',
+    }
+
+    it('should handle initial state', () => {
+        expect(userReducer(undefined, {type: "unknown"})).toEqual(initialState);
+    });
+
+    it('should handle flag clearing', () => {
+        expect(userReducer(testState, clearState())).toEqual(initialState);
+    })
+});
